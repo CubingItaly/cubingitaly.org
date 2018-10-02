8f5c51b652a5c236ef6c475e0f2fab758025db5c
Matteo Colombo <matte.colo@gmail.com>
2018-10-02 10:49:02 +0200

    Improve image resizing

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  banner: string = "../../../assets/images/banner.jpg";
+  banner_array: string[] = ["../../../assets/images/banner.jpg"];
   constructor() { }
 
   ngOnInit() {
