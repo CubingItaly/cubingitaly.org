a348a4535fed0c70dcbaed89c63e4044460bbc51
Matteo Colombo <matte.colo@gmail.com>
2018-10-18 17:25:54 +0200

    Restore login with staging for dev

--- server/passport/strategy.passport.wca.ts
+++ server/passport/strategy.passport.wca.ts
@@ -7,21 +7,42 @@ import { UserRepository } from '../db/repository/user.repository';
 import { getCustomRepository } from 'typeorm';
 import { UserEntity } from '../db/entity/user.entity';
 
-passport.use(new WCAStrategy({
-    clientID: keys.wca.client_id,
-    clientSecret: keys.wca.client_secret,
-    callbackURL: keys.wca.redirect_uri,
-    scope: keys.wca.scope,
-    userAgent: keys.wca.user_agent
-},
-    async (accessToken, refreshToken, profile, done) => {
-        const user: UserModel = Deserialize(profile._json.me, UserModel);
-        const userRepo: UserRepository = getCustomRepository(UserRepository);
-        let dbUser: UserEntity = new UserEntity();
-        dbUser._assimilate(user);
-        dbUser = await userRepo.updateUser(dbUser);
-        done(null, user);
-    }));
+if (process.env.NODE_ENV === "production") {
+    passport.use(new WCAStrategy({
+        clientID: keys.wca.prod.client_id,
+        clientSecret: keys.wca.prod.client_secret,
+        callbackURL: keys.wca.prod.redirect_uri,
+        scope: keys.wca.prod.scope,
+        userAgent: keys.wca.prod.user_agent
+    },
+        async (accessToken, refreshToken, profile, done) => {
+            const user: UserModel = Deserialize(profile._json.me, UserModel);
+            const userRepo: UserRepository = getCustomRepository(UserRepository);
+            let dbUser: UserEntity = new UserEntity();
+            dbUser._assimilate(user);
+            dbUser = await userRepo.updateUser(dbUser);
+            done(null, user);
+        }));
+} else {
+    passport.use(new WCAStrategy({
+        clientID: keys.wca.dev.client_id,
+        clientSecret: keys.wca.dev.client_secret,
+        callbackURL: keys.wca.dev.redirect_uri,
+        scope: keys.wca.dev.scope,
+        userAgent: keys.wca.dev.user_agent,
+        authorizationURL: 'https://staging.worldcubeassociation.org/oauth/authorize',
+        tokenURL: 'https://staging.worldcubeassociation.org/oauth/token',
+        userProfileURL: 'https://staging.worldcubeassociation.org/api/v0/me'
+    },
+        async (accessToken, refreshToken, profile, done) => {
+            const user: UserModel = Deserialize(profile._json.me, UserModel);
+            const userRepo: UserRepository = getCustomRepository(UserRepository);
+            let dbUser: UserEntity = new UserEntity();
+            dbUser._assimilate(user);
+            dbUser = await userRepo.updateUser(dbUser);
+            done(null, user);
+        }));
+}
 
 
 passport.serializeUser((user: UserModel, done) => {
